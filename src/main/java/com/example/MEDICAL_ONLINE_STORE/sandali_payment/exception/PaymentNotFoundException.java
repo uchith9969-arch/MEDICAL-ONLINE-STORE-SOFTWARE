adf1b996386cd949a76ef4e224d6d1fd8f5c8399
IT25103962 <IT25103962@my.sliit.lk>
2026-05-23 03:54:25 +0530

    exception file for payment error handeling

---
+++ src/main/java/com/example/MEDICAL_ONLINE_STORE/sandali_payment/exception/PaymentNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.MEDICAL_ONLINE_STORE.sandali_payment.exception;
+
+public class PaymentNotFoundException extends Exception {
+
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
