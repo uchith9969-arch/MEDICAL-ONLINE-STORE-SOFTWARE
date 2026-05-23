b9f2adbec31f278ba3b8c8ad7ed747a41e923540
UV Ranasinghe <Uchith9969@gmail.com>
2026-05-23 09:35:43 +0530

    adding the model file- MedicineCategory

---
+++ src/main/java/com/example/MEDICAL_ONLINE_STORE/samod_medicine/model/MedicineCategory.java
@@ -0,0 +1,11 @@
+package com.example.MEDICAL_ONLINE_STORE.samod_medicine.model;
+
+public enum MedicineCategory {
+    TABLET,
+    CAPSULE,
+    SYRUP,
+    INJECTION,
+    CREAM,
+    DROPS,
+    OTHER
+}
