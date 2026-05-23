baf84bbeb8b8d5993c1788cf64bd3751aab2aa12
UV Ranasinghe <Uchith9969@gmail.com>
2026-05-23 09:33:40 +0530

    Exception to handle the file errors

---
+++ src/main/java/com/example/MEDICAL_ONLINE_STORE/samod_medicine/exception/MedicineNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.MEDICAL_ONLINE_STORE.samod_medicine.exception;
+
+public class MedicineNotFoundException extends Exception {
+
+    public MedicineNotFoundException(String message) {
+        super(message);
+    }
+}
