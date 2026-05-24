2731a4ee8720e756f2185a1a87510d701807095e
UV Ranasinghe <Uchith9969@gmail.com>
2026-05-24 13:11:31 +0530

    OrderNotFoundException for file error handling

---
+++ src/main/java/com/example/MEDICAL_ONLINE_STORE/thisen_order/exception/OrderNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.MEDICAL_ONLINE_STORE.thisen_order.exception;
+
+public class OrderNotFoundException extends Exception {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+}
